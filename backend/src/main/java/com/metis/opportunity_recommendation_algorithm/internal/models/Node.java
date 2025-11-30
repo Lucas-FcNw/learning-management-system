8f120e3af5aa55a836af202004fee3e5e4524b75
Lucas Fernandes <Lucas.Fernandes.Camargo@proton.me>
2025-11-30 00:03:31 -0300

    Fix: Adiciona mocks para sistema de recomendacao

--- backend/src/main/java/com/metis/opportunity_recommendation_algorithm/internal/models/Node.java
+++ backend/src/main/java/com/metis/opportunity_recommendation_algorithm/internal/models/Node.java
@@ -1,5 +1,6 @@
 package com.metis.opportunity_recommendation_algorithm.internal.models;
 
+import com.metis.opportunity_recommendation_algorithm.internal.models.enums.NodeType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,5 +10,5 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Node {
     private String id;
-    private String type;
+    private NodeType type; // Agora usa o Enum correto em vez de String
 }
