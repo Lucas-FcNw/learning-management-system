3c7d1d09d083eed6a63899ecd9671ef66d38a7bd
Lucas Fernandes <Lucas.Fernandes.Camargo@proton.me>
2025-11-30 00:06:37 -0300

    Fix: Adiciona mocks para sistema de recomendacao

---
+++ backend/src/main/java/com/metis/opportunity_recommendation_algorithm/internal/models/enums/RelationType.java
@@ -0,0 +1,9 @@
+package com.metis.opportunity_recommendation_algorithm.internal.models.enums;
+
+public enum RelationType {
+    INTERESTED_IN,
+    HAS_SKILL,
+    REQUIRES_SKILL,
+    PARTICIPATES_IN,
+    RELATED_TO
+}
